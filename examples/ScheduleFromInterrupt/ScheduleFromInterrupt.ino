54c280c0394d99f79a693b17d1f153b15c1d042c
Peter Rosenberg <p.rosenberg@gmx.net>
2018-03-04 20:55:45 +1100

    adjust examples to esp32

--- examples/ScheduleFromInterrupt/ScheduleFromInterrupt.ino
+++ examples/ScheduleFromInterrupt/ScheduleFromInterrupt.ino
@@ -1,7 +1,12 @@
 #include <DeepSleepScheduler.h>
 
+#ifdef ESP32
+#define INTERRUPT_PIN 4
+#define LED_PIN 2
+#else
 #define INTERRUPT_PIN 2
 #define LED_PIN 13
+#endif
 
 void ledOn() {
   digitalWrite(LED_PIN, HIGH);
@@ -17,6 +22,11 @@ void isrInterruptPin() {
 }
 
 void setup() {
+#ifdef ESP32
+  // wake up using ext0 (4 low)
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_4, LOW);
+#endif
+
   pinMode(INTERRUPT_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), isrInterruptPin, FALLING);
   pinMode(LED_PIN, OUTPUT);
